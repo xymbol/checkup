d3226c0423d578d31ebdacfaec93bb3d5107a0e3
Adrian Mugnolo <adrian@mugnolo.com>
2015-11-24 22:56:55 -0300

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Place.create([
-  { url: 'http://www.resultados.gob.ar/bltgetelegr/01/001/0001/010010001_0001.htm' },
-  { url: 'http://www.resultados.gob.ar/bltgetelegr/01/001/0001/010010001_0002.htm' },
-  { url: 'http://www.resultados.gob.ar/bltgetelegr/01/001/0001/010010001_0003.htm' }
+Place.create!([
+  {
+    url: 'http://www.resultados.gob.ar/bltgetelegr/01/001/0001/010010001_0001.htm',
+    code: '4f06b25303a3099a32e473f149de5f13'
+  },
+  {
+    url: 'http://www.resultados.gob.ar/bltgetelegr/01/001/0001/010010001_0002.htm',
+    code: '2cbef42e707fec877093013c3d79b3b4'
+  },
+  {
+    url: 'http://www.resultados.gob.ar/bltgetelegr/01/001/0001/010010001_0003.htm',
+    code: '454aef1f2161124e8d51a87efab70384'
+  }
 ])
