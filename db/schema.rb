57a81c993a62f00299cb563660d75073e159413c
Adrian Mugnolo <adrian@mugnolo.com>
2015-11-26 18:59:47 -0300

    Add email to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151125193620) do
+ActiveRecord::Schema.define(version: 20151126183705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20151125193620) do
     t.string   "uid"
     t.string   "nickname"
     t.string   "name"
+    t.string   "email"
   end
 
   add_index "users", ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true, using: :btree
